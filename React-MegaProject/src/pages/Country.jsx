cdb6bd6263a44e9b3719fcf26933c30d5c717454
Faraz Ayaz <farazayazkhan1@gmail.com>
2025-08-19 17:16:58 +0500

    country details done

--- React-MegaProject/src/pages/Country.jsx
+++ React-MegaProject/src/pages/Country.jsx
@@ -1,32 +1,51 @@
 import { useEffect, useState, useTransition } from "react";
 import { getCountryData } from "../api/PostApi";
 import { Loader } from "../components/ui/Loader";
-import {CountryCard} from "../components/Layout/CountryCard";
-
+import { CountryCard } from "../components/Layout/CountryCard";
+import SearchFilter from "../components/ui/SearchFilter";
 
 const Country = () => {
   const [isPending, startTransition] = useTransition();
   const [countries, setCountries] = useState([]);
-  useEffect(()=>{
-    startTransition(async() =>{
-      const res = await getCountryData();
-      console.log(res.data);
-      setCountries(res.data);
-    })
-  },[])
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  useEffect(() => {
+    startTransition(async () => {
+      try {
+        const res = await getCountryData();
+        setCountries(res.data);
+      } catch (error) {
+        console.error("Failed to fetch countries:", error);
+      }
+    });
+  }, []);
 
   if (isPending) {
     return <Loader />;
   }
+  const filterCountries = countries.filter((country) => {
+    return (
+      (!search ||
+        country.name.common.toLowerCase().includes(search.toLowerCase())) &&
+      (filter.toLowerCase() === "all" || country.region === filter)
+    );
+  });
 
   return (
     <section className="country-section">
+      <SearchFilter
+        search={search}
+        setSearch={setSearch}
+        filter={filter}
+        setFilter={setFilter}
+        countries={countries}
+        setCountries={setCountries}
+      />
+
       <ul className="grid grid-four-cols">
-        {
-         countries.map((curCountry, index)=>{
-          return <CountryCard Country={curCountry} key={index}  />
-         })
-        }
+        {filterCountries.map((curCountry) => (
+          <CountryCard Country={curCountry} key={curCountry.name.common} />
+        ))}
       </ul>
     </section>
   );
